7f87d6cbcd65f09f4ffdb0b284c14673f4779635
kevashans <kevashans2003@gmail.com>
2023-10-29 17:59:18 +0000

    added hardcoded leaderboard design

--- app/src/main/java/com/example/scar/ui/theme/Color.kt
+++ app/src/main/java/com/example/scar/ui/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val cardBg= Color(0xFF1E1E1E)
+val mainBg = Color(0xFF3E316F)

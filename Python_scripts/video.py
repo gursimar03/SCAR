cc94b66e8e962e1e6fd8d3e9188c114c72e5412c
Josef Žemlička <almezj01@gmail.com>
2023-11-18 15:05:48 +0000

    Added mediapipe for better human body recognition

--- Python_scripts/video.py
+++ Python_scripts/video.py
@@ -1,10 +1,14 @@
 import cv2
 import numpy as np
+import mediapipe as mp
+import math as Math
 
 cap = cv2.VideoCapture(0)
 
-# Use full body haarcascade
-BodyCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fullbody.xml')
+# Load the mediapipe pose model
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+mpDraw = mp.solutions.drawing_utils
 
 # Get the width and height of the frame
 ret, frame = cap.read()
@@ -14,38 +18,64 @@ height, width, _ = frame.shape
 center = (width // 2, height // 2)
 radius = min(width, height) // 4
 
-while True:
+while cap.isOpened():
     ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    bodies = BodyCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    if not ret:
+        break
+
+    # Convert the BGR image to RGB
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Process the image and detect bodies
+    results = pose.process(image)
+
+    # Draw the pose annotations on the image
+    annotatedImage = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(annotatedImage, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
 
     # Draw the big red circle in the middle of the screen
-    cv2.circle(frame, center, radius, (0, 0, 255), 2)
+    cv2.circle(annotatedImage, center, radius, (0, 0, 255), 2)
 
-    for (x, y, w, h) in bodies:
-        # Calculate the center of the detected body
-        body_center = (x + w // 2, y + h // 2)
+    # Check if a body is detected
+    if results.pose_landmarks:
 
-        # Check if the body is inside the circle
+        # Set the body center to the nose landmark
+        body_center = (int(results.pose_landmarks.landmark[mpPose.PoseLandmark.NOSE].x * width),
+                       int(results.pose_landmarks.landmark[mpPose.PoseLandmark.NOSE].y * height))
+
+        # Check if the body center is inside the circle
         inside = (body_center[0] - center[0]) ** 2 + (body_center[1] - center[1]) ** 2 < radius ** 2
+        
+        # Setup the led indicators
+        ledIndicators = [(center[0] + radius * Math.cos(i * Math.pi / 4), center[1] + radius * Math.sin(i * Math.pi / 4)) for i in range(8)]
 
         if inside:
             # If the body is inside the circle, light up the whole border of the window green
-            cv2.rectangle(frame, (0, 0), (width - 1, height - 1), (0, 255, 0), 10)
+            for i in range(8):
+                color = (0, 255, 0)
+                cv2.circle(annotatedImage, (int(ledIndicators[i][0]), int(ledIndicators[i][1])), 10, color, -1)
         else:
-            # If the body is outside the circle, light up the closest side green
-            distances = [body_center[0], width - body_center[0], body_center[1], height - body_center[1]]
-            closest_side = distances.index(min(distances))
-            if closest_side == 0:
-                cv2.line(frame, (0, 0), (0, height - 1), (0, 255, 0), 10)
-            elif closest_side == 1:
-                cv2.line(frame, (width - 1, 0), (width - 1, height - 1), (0, 255, 0), 10)
-            elif closest_side == 2:
-                cv2.line(frame, (0, 0), (width - 1, 0), (0, 255, 0), 10)
-            else:
-                cv2.line(frame, (0, height - 1), (width - 1, height - 1), (0, 255, 0), 10)
+            # If the body is outside the circle, 8 black dots are drawn on the circle
+            ## to indicate the 8 segments of the circle
+            ### The 3 closest dots will be lit up blue
 
-    cv2.imshow('Body Detection', frame)
+            # Calculate the distances from the body center to each LED indicator
+            distances = [((body_center[0] - led[0]) ** 2 + (body_center[1] - led[1]) ** 2) ** 0.5 for led in ledIndicators]
+
+            # Get the indices of the 3 closest LED indicators
+            closest_leds = sorted(range(len(distances)), key=lambda i: distances[i])[:3]
+
+            # Draw the LED indicators
+            for i in range(8):
+                color = (255, 0, 0) if i in closest_leds else (34, 34, 34)
+                cv2.circle(annotatedImage, (int(ledIndicators[i][0]), int(ledIndicators[i][1])), 10, color, -1)
+
+            
+
+            
+
+    cv2.imshow('Body Detection', annotatedImage)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break

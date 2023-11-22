259880f6888ca32c3c0c28fac1353b663a5b546b
gursimar03 <gursimar03@gmail.com>
2023-11-22 20:50:15 +0000

    backend endpoint files created

---
+++ webApp_api/models/arena.py
@@ -0,0 +1,10 @@
+#
+
+from app_factory import db
+
+class Arena(db.Model):
+    arena_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    arena_name = db.Column(db.String(50), nullable=False)
+
+    # Relationships
+    match_results = db.relationship('MatchResult', backref='arena', lazy=True)

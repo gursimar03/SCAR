0584974d5343700f79a48a137037e9f7335004f4
gursimar03 <gursimar03@gmail.com>
2023-11-25 21:25:44 +0000

    Server code added to deployed_webserver

--- webApp_api/app.py
+++ webApp_api/app.py
@@ -1,14 +1,18 @@
 from flask import render_template , jsonify
 from app_factory import create_app
 from auth.user import user_bp
-from routes import *
+from routes import leaderboard_bp, arena_bp, inventory_bp, match_result_bp, weapon_bp
 import os 
 
 app, db = create_app()
 
 # Set the connector for each blueprint
 app.register_blueprint(user_bp)
-
+app.register_blueprint(leaderboard_bp)
+app.register_blueprint(arena_bp)
+app.register_blueprint(inventory_bp)
+app.register_blueprint(match_result_bp)
+app.register_blueprint(weapon_bp)
 
 def access_token_verify(access_token):
     verifier = os.environ.get('access_token')

344a783586aad29f2b231a5f32ebb082ec1a88bb
almezj <almezj01@gmail.com>
2024-01-13 19:40:55 +0000

    Added admin token check for registration endpoint and basic session management

--- deployed_webserver/SCAR/SCAR.wsgi
+++ deployed_webserver/SCAR/SCAR.wsgi
@@ -7,6 +7,7 @@ sys.path.insert(0,"/var/www/SCAR/")
 os.environ['TEST'] = 'test'
 os.environ['FACEBOOK_APP'] = 'Your facebook app id'
 os.environ['FACEBOOK_SECRET']='Your facebook app secret'
+os.environ['admin_token']='08eff29c780d53adc819e095b2b0f0fdbe88862cafafc9523642124cc5492672'
 
 def application(environ, start_response):
  for key in ['TEST', 'FACEBOOK_APP', 'FACEBOOK_SECRET']:
--- deployed_webserver/SCAR/SCAR/auth/user.py
+++ deployed_webserver/SCAR/SCAR/auth/user.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify, request
+import os
+from flask import Blueprint, jsonify, request, flask
 from SCAR.app_factory import db
 from SCAR.models.user import User
 from SCAR.models.session import Session
@@ -26,6 +27,13 @@ def google_register_user():
 
 @user_bp.route('/api/register', methods=['POST'])
 def register_user():
+    auth_header = request.headers.get('Authorization')
+    admin_token = os.environ.get('admin_token')
+    
+    if not auth_header or auth_header != admin_token:
+        return jsonify({'success': False, 'message': 'Invalid Authorization header'}), 401
+
+   
     data = request.json
     email = data.get('email')
     password = data.get('password')
@@ -60,8 +68,7 @@ def login_user():
     if user is None or not bcrypt.check_password_hash(user.password, password):
         return jsonify({'success': False, 'message': 'Invalid email or password'}), 401
 
-    # Add your user login logic here, including creating and storing a session
-    # ...
+    flask.current_user = user
 
     return jsonify({
         'success': True,
